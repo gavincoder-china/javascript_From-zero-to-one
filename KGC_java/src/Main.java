7b38a29dafff49cc19acf7a890827aeb1f2586f2
GAVINCODER\Administrator <907516241@qq.com>
2018-07-30 10:12:02 +0800

    ..

--- KGC_java/src/Main.java
+++ KGC_java/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello \n World!");
+        char x='a';
+
+        System.out.println(x);
+
+
     }
 }
